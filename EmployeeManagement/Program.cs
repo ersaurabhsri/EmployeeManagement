f2674261a6ecd251b56d8bcefb1ca27d394def29
Saurabh Shriwastav <Saurabh.S@sflhub.com>
2023-07-11 15:26:28 +0530

    commit

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//adding something for test purpose
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
